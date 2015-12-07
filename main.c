c00d11397847d6f3fc829c80b77e5d60d65e1a35
halogenandtoast <halogenandtoast@gmail.com>
2015-12-07 12:05:51 -0500

    Allow file input

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
 extern int yylex();
+extern FILE* yyin;
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    yyin = fopen(argv[1], "r");
+  }
   yylex();
   return 0;
 }
